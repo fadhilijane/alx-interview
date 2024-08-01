2059851701457bba4147c86067c9d023c40b54cf
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2024-08-01 18:21:15 +0300

    Update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -9,14 +9,14 @@ def canUnlockAll(boxes):
     box is unlocked.
     '''
     n = len(boxes)
-    seen_boxes = set([0])
-    unseen_boxes = set(boxes[0]).difference(set([0]))
-    while len(unseen_boxes) > 0:
-        boxIdx = unseen_boxes.pop()
-        if not boxIdx or boxIdx >= n or boxIdx < 0:
+    seenBoxes = set([0])
+    unseenBoxes = set(boxes[0]).difference(set([0]))
+    while len(unseenBoxes) > 0:
+        boxId = unseenBoxes.pop()
+        if not boxId or boxId >= n or boxId < 0:
             continue
-        if boxIdx not in seen_boxes:
-            unseen_boxes = unseen_boxes.union(boxes[boxIdx])
-            seen_boxes.add(boxIdx)
-    return n == len(seen_boxes)
+        if boxId not in seen_boxes:
+            unseenBoxes = unseenBoxes.union(boxes[boxId])
+            seenBoxes.add(boxId)
+    return n == len(seenBoxes)
 
